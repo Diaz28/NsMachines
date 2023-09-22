b4167f3cfaa5a771aaa8295d9624386805354156
Andre <andref@ua.pt>
2023-09-22 23:05:42 +0100

    structured the files to use react router #2

--- ns_machines/src/index.jsx
+++ ns_machines/src/index.jsx
@@ -1,30 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
 import './index.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import ErrorPage from "./error-page";
-import Root from "./routes/root";
+import LandingPage from "./routes/landingPage";
 
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-        <RouterProvider router={router} />
-  </React.StrictMode>
+  <BrowserRouter>
+    <div>
+
+      <Navbar expand="lg" className="bg-body-tertiary sticky-top">
+        <Container fluid>
+          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
+              <NavDropdown title="Link" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#" disabled>
+                Link
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+
+
+      <Routes>
+        <Route index element={<LandingPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+
+
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top fixed-bottom">
+        <div class="col-md-4 d-flex align-items-center">
+          <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+            <svg class="bi" width="30" height="24"><use xlinkhref="#bootstrap" /></svg>
+          </a>
+          <span class="mb-3 mb-md-0 text-body-secondary">&copy; 2023 Company, Inc</span>
+        </div>
+
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlinkhref="#twitter" /></svg></a></li>
+          <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlinkhref="#instagram" /></svg></a></li>
+          <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlinkshref="#facebook" /></svg></a></li>
+        </ul>
+      </footer>
+
+    </div>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- ns_machines/src/routes/landingPage.jsx
+++ ns_machines/src/routes/landingPage.jsx
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 export default function Root() {
     return (
         <div>
+
             <Carousel>
                 <Carousel.Item interval={5000}>
                     <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)" /></svg>
@@ -75,25 +76,11 @@ export default function Root() {
 
             </div>
 
-            <div class="container">
-                <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                    <div class="col-md-4 d-flex align-items-center">
-                        <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-                            <svg class="bi" width="30" height="24"><use xlinkhref="#bootstrap" /></svg>
-                        </a>
-                        <span class="mb-3 mb-md-0 text-body-secondary">&copy; 2023 Company, Inc</span>
-                    </div>
-
-                    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-                        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlinkhref="#twitter" /></svg></a></li>
-                        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlinkhref="#instagram" /></svg></a></li>
-                        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlinkshref="#facebook" /></svg></a></li>
-                    </ul>
-                </footer>
-            </div>
 
         </div>
 
 
     );
 }
+
+
